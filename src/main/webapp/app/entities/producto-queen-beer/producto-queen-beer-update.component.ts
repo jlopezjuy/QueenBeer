50ceb83d15231f7e2d7d42856f12f04c94aeeec6
Juan Manuel Lopez <jlopezjuy@gmail.com>
2019-03-03 09:19:09 -0300

    clean navbar

--- src/main/webapp/app/entities/producto-queen-beer/producto-queen-beer-update.component.ts
+++ src/main/webapp/app/entities/producto-queen-beer/producto-queen-beer-update.component.ts
@@ -6,6 +6,8 @@ import { filter, map } from 'rxjs/operators';
 import { JhiDataUtils } from 'ng-jhipster';
 import { IProductoQueenBeer } from 'app/shared/model/producto-queen-beer.model';
 import { ProductoQueenBeerService } from './producto-queen-beer.service';
+import { Envase, IEnvase } from 'app/shared/model/envase.model';
+import { EnvaseService } from 'app/entities/envase';
 
 @Component({
     selector: 'jhi-producto-queen-beer-update',
@@ -13,12 +15,16 @@ import { ProductoQueenBeerService } from './producto-queen-beer.service';
 })
 export class ProductoQueenBeerUpdateComponent implements OnInit {
     producto: IProductoQueenBeer;
+    envase: IEnvase;
+    envases: IEnvase[];
     isSaving: boolean;
+    isSavindEnvase: boolean;
 
     constructor(
         protected dataUtils: JhiDataUtils,
         protected productoService: ProductoQueenBeerService,
         protected elementRef: ElementRef,
+        private envaseService: EnvaseService,
         protected activatedRoute: ActivatedRoute
     ) {}
 
@@ -26,7 +32,12 @@ export class ProductoQueenBeerUpdateComponent implements OnInit {
         this.isSaving = false;
         this.activatedRoute.data.subscribe(({ producto }) => {
             this.producto = producto;
+            this.envase = new Envase();
+            if (this.producto.id) {
+                this.loadEnvases(this.producto.id);
+            }
         });
+        this.envases = [];
     }
 
     byteSize(field) {
@@ -52,14 +63,23 @@ export class ProductoQueenBeerUpdateComponent implements OnInit {
     save() {
         this.isSaving = true;
         if (this.producto.id !== undefined) {
+            this.isSavindEnvase = false;
             this.subscribeToSaveResponse(this.productoService.update(this.producto));
         } else {
+            this.isSavindEnvase = true;
             this.subscribeToSaveResponse(this.productoService.create(this.producto));
         }
     }
 
     protected subscribeToSaveResponse(result: Observable<HttpResponse<IProductoQueenBeer>>) {
-        result.subscribe((res: HttpResponse<IProductoQueenBeer>) => this.onSaveSuccess(), (res: HttpErrorResponse) => this.onSaveError());
+        result.subscribe(
+            (res: HttpResponse<IProductoQueenBeer>) => {
+                this.onSaveSuccess();
+                console.log(res);
+                this.saveEnvases(res.body);
+            },
+            (res: HttpErrorResponse) => this.onSaveError()
+        );
     }
 
     protected onSaveSuccess() {
@@ -69,5 +89,40 @@ export class ProductoQueenBeerUpdateComponent implements OnInit {
 
     protected onSaveError() {
         this.isSaving = false;
+    }
+
+    addEnvase() {
+        this.envases.push(this.envase);
+        this.envase = new Envase();
+    }
+
+    protected saveEnvases(producto: IProductoQueenBeer) {
+        this.envases.forEach(envase => {
+            if (this.isSavindEnvase) {
+                envase.productoId = producto.id;
+                this.envaseService.create(envase).subscribe(resp => {
+                    console.log('create');
+                });
+            } else {
+                envase.productoId = producto.id;
+                this.envaseService.update(envase).subscribe(
+                    resp => {
+                        console.log('update');
+                    },
+                    error => {
+                        this.envaseService.create(envase).subscribe(resp => {
+                            console.log('create');
+                        });
+                    }
+                );
+            }
+        });
+    }
+
+    protected loadEnvases(productoId: number) {
+        console.log('Producto Id: ' + productoId);
+        this.envaseService.queryByProductoId(productoId).subscribe(resp => {
+            this.envases = resp.body;
+        });
     }
 }
